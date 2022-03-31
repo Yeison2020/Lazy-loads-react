032174bb4c44ebafc2085b0171e056fc55a13ea3
Yeison <enmanuelquiterios@gmail.com>
2022-03-31 10:35:03 -0400

    Added react-router-dom v6

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,45 +1,63 @@
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
-  NavLink
-} from 'react-router-dom';
+  NavLink,
+} from "react-router-dom";
 
-import logo from '../logo.svg';
+import logo from "../logo.svg";
 
 export const Navigation = () => {
   return (
     <Router>
       <div className="main-layout">
         <nav>
-            <img src={ logo } alt="React Logo" />
+          <img src={logo} alt="React Logo" />
           <ul>
             <li>
-              <NavLink to="/" activeClassName="nav-active" exact>Home</NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  "nav-active" + (isActive ? "active" : "")
+                }
+                end={true}
+                to="/home"
+              >
+                Home
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  "nav-active" + (isActive ? "active" : "")
+                }
+                end={true}
+                to="/about"
+              >
+                About
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  "nav-active" + (isActive ? "active" : "")
+                }
+                end={true}
+                to="/users"
+              >
+                Users
+              </NavLink>
             </li>
           </ul>
         </nav>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <h1>About</h1>
-          </Route>
-          <Route path="/users">
-            <h1>Users</h1>
-          </Route>
-          <Route path="/">
-            <h1>Home</h1>
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/about" element={<h1>About</h1>}></Route>
+          <Route path="/users" element={<h1>Users</h1>}></Route>
+          <Route path="/home" element={<h1>Home</h1>}></Route>
+        </Routes>
       </div>
     </Router>
   );
-}
+};
