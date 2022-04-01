1bf06ac194c18b2dc9885567ebe4621fe90409b4
Yeison <enmanuelquiterios@gmail.com>
2022-04-01 12:55:36 -0400

    Working with lazy and no lazy

--- src/01-lazyload/layout/Lazylayout.tsx
+++ src/01-lazyload/layout/Lazylayout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink, Routes, Route } from "react-router-dom";
+import { NavLink, Routes, Route, Navigate } from "react-router-dom";
 import { LazyPage1, LazyPage2, LazyPage3 } from "../pages";
 const Lazylayout = () => {
   return (
@@ -22,6 +22,8 @@ const Lazylayout = () => {
         <Route path="lazy1" element={<LazyPage1 />} />
         <Route path="lazy2" element={<LazyPage2 />} />
         <Route path="lazy3" element={<LazyPage3 />} />
+        {/* <Route path="*" element={<div>No Found</div>} /> */}
+        <Route path="*" element={<Navigate replace to="lazy1" />} />
       </Routes>
     </div>
   );
